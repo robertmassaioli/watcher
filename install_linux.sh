94b0e777b19cedbfe46c6c87de459dfa105a8e9e
Robert Massaioli <robertmassaioli@gmail.com>
2011-05-13 12:02:25 +1000

    Added in the actual install line into the install script.

--- install_linux.sh
+++ install_linux.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+INSTALL_FOLDER=/usr/local/bin
+WATCHER_PROGRAM=watcher.bash
+
 # If you have your own video player that you would like to use then do not install mplayer.
 sudo apt-get install coreutils grep mplayer
+sudo cp "$WATCHER_PROGRAM" "$INSTALL_FOLDER/watcher"
